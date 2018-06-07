f09f42db5c765af36a6bda7c709082f16132ebce
梅村拓朗 <takuroumemura@umemuratakurounoMacBook-Pro.local>
2018-06-07 19:42:54 +0900

    Bacckend 03
    エラー問題の方が狂ったのでそっちも直しました

--- app/models/author.rb
+++ app/models/author.rb
@@ -3,4 +3,25 @@ has_many :books
   def self.created_after(target_date)
     self.where("created_at > ?", target_date)
   end
+
+composed_of :fullname,
+       :class_name => "FullName",
+       :mapping => [
+       [:family_name, :family_name],
+       [:first_name, :first_name]
+       ]
+end
+
+class FullName
+  attr_reader :family_name, :first_name
+
+  def initialize(family_name, first_name)
+    @family_name = family_name
+    @first_name = first_name
+  end
+
+  def to_s
+    [@family_name, @first_name].compact.join("")
+  end
+
 end
